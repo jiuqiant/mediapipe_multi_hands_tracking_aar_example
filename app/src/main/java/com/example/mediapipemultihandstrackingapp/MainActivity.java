356ce08fd699089591730f202a0030a110c0708c
Jiuqiang Tang <jqtang@google.com>
2021-01-21 20:58:35 -0800

    update to match mediapipe v0.8.2

--- app/src/main/java/com/example/mediapipemultihandstrackingapp/MainActivity.java
+++ app/src/main/java/com/example/mediapipemultihandstrackingapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.mediapipemultihandstrackingapp;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.util.Log;
 import android.util.Size;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmarkList;
 import com.google.mediapipe.components.CameraHelper;
@@ -17,27 +23,39 @@ import com.google.mediapipe.components.ExternalTextureConverter;
 import com.google.mediapipe.components.FrameProcessor;
 import com.google.mediapipe.components.PermissionHelper;
 import com.google.mediapipe.framework.AndroidAssetUtil;
+import com.google.mediapipe.framework.AndroidPacketCreator;
 import com.google.mediapipe.framework.PacketGetter;
+import com.google.mediapipe.framework.Packet;
 import com.google.mediapipe.glutil.EglManager;
+
+import java.util.HashMap;
 import java.util.List;
-/** Main activity of MediaPipe example apps. */
+import java.util.Map;
+
+/**
+ * Main activity of MediaPipe example apps.
+ */
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private static final String BINARY_GRAPH_NAME = "multi_hand_tracking_mobile_gpu.binarypb";
+    private static final String BINARY_GRAPH_NAME = "hand_tracking_mobile_gpu.binarypb";
     private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
     private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
-    private static final String OUTPUT_LANDMARKS_STREAM_NAME = "multi_hand_landmarks";
+    private static final String OUTPUT_LANDMARKS_STREAM_NAME = "hand_landmarks";
+    private static final String INPUT_NUM_HANDS_SIDE_PACKET_NAME = "num_hands";
+    private static final int NUM_HANDS = 2;
     private static final CameraHelper.CameraFacing CAMERA_FACING = CameraHelper.CameraFacing.FRONT;
     // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
     // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
     // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
     // corner, whereas MediaPipe in general assumes the image origin is at top-left.
     private static final boolean FLIP_FRAMES_VERTICALLY = true;
+
     static {
         // Load all native libraries needed by the app.
         System.loadLibrary("mediapipe_jni");
         System.loadLibrary("opencv_java3");
     }
+
     // {@link SurfaceTexture} where the camera-preview frames can be accessed.
     private SurfaceTexture previewFrameTexture;
     // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
@@ -50,14 +68,26 @@ public class MainActivity extends AppCompatActivity {
     // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
     // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
     private ExternalTextureConverter converter;
+    // ApplicationInfo for retrieving metadata defined in the manifest.
+    private ApplicationInfo applicationInfo;
     // Handles camera access via the {@link CameraX} Jetpack support library.
     private CameraXPreviewHelper cameraHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(getContentViewLayoutResId());
+
+        try {
+            applicationInfo =
+                    getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+        } catch (NameNotFoundException e) {
+            Log.e(TAG, "Cannot find application info: " + e);
+        }
+
         previewDisplayView = new SurfaceView(this);
         setupPreviewDisplayView();
+
         // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
         // binary graphs.
         AndroidAssetUtil.initializeNativeAssetManager(this);
@@ -69,47 +99,119 @@ public class MainActivity extends AppCompatActivity {
                         BINARY_GRAPH_NAME,
                         INPUT_VIDEO_STREAM_NAME,
                         OUTPUT_VIDEO_STREAM_NAME);
-        processor.getVideoSurfaceOutput().setFlipY(FLIP_FRAMES_VERTICALLY);
-        processor.addPacketCallback(
-                OUTPUT_LANDMARKS_STREAM_NAME,
-                (packet) -> {
-                    Log.d(TAG, "Received multi-hand landmarks packet.");
-                    List<NormalizedLandmarkList> multiHandLandmarks =
-                            PacketGetter.getProtoVector(packet, NormalizedLandmarkList.parser());
-                    Log.d(
-                            TAG,
-                            "[TS:"
-                                    + packet.getTimestamp()
-                                    + "] "
-                                    + getMultiHandLandmarksDebugString(multiHandLandmarks));
-                });
+        processor
+                .getVideoSurfaceOutput()
+                .setFlipY(FLIP_FRAMES_VERTICALLY);
+
         PermissionHelper.checkAndRequestCameraPermissions(this);
+        AndroidPacketCreator packetCreator = processor.getPacketCreator();
+        Map<String, Packet> inputSidePackets = new HashMap<>();
+        inputSidePackets.put(INPUT_NUM_HANDS_SIDE_PACKET_NAME, packetCreator.createInt32(NUM_HANDS));
+        processor.setInputSidePackets(inputSidePackets);
+
+        // To show verbose logging, run:
+        // adb shell setprop log.tag.MainActivity VERBOSE
+        if (Log.isLoggable(TAG, Log.VERBOSE)) {
+            processor.addPacketCallback(
+                    OUTPUT_LANDMARKS_STREAM_NAME,
+                    (packet) -> {
+                        Log.v(TAG, "Received multi-hand landmarks packet.");
+                        List<NormalizedLandmarkList> multiHandLandmarks =
+                                PacketGetter.getProtoVector(packet, NormalizedLandmarkList.parser());
+                        Log.v(
+                                TAG,
+                                "[TS:"
+                                        + packet.getTimestamp()
+                                        + "] "
+                                        + getMultiHandLandmarksDebugString(multiHandLandmarks));
+                    });
+        }
     }
+
+    // Used to obtain the content view for this application. If you are extending this class, and
+    // have a custom layout, override this method and return the custom layout.
+    protected int getContentViewLayoutResId() {
+        return R.layout.activity_main;
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
-        converter = new ExternalTextureConverter(eglManager.getContext());
+        converter =
+                new ExternalTextureConverter(
+                        eglManager.getContext(), 2);
         converter.setFlipY(FLIP_FRAMES_VERTICALLY);
         converter.setConsumer(processor);
         if (PermissionHelper.cameraPermissionsGranted(this)) {
             startCamera();
         }
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         converter.close();
+
+        // Hide preview display until we re-open the camera again.
+        previewDisplayView.setVisibility(View.GONE);
     }
+
     @Override
     public void onRequestPermissionsResult(
             int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+    protected void onCameraStarted(SurfaceTexture surfaceTexture) {
+        previewFrameTexture = surfaceTexture;
+        // Make the display view visible to start showing the preview. This triggers the
+        // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+        previewDisplayView.setVisibility(View.VISIBLE);
+    }
+
+    protected Size cameraTargetResolution() {
+        return null; // No preference and let the camera (helper) decide.
+    }
+
+    public void startCamera() {
+        cameraHelper = new CameraXPreviewHelper();
+        cameraHelper.setOnCameraStartedListener(
+                surfaceTexture -> {
+                    onCameraStarted(surfaceTexture);
+                });
+        CameraHelper.CameraFacing cameraFacing = CameraHelper.CameraFacing.FRONT;
+        cameraHelper.startCamera(
+                this, cameraFacing, /*unusedSurfaceTexture=*/ null, cameraTargetResolution());
+    }
+
+    protected Size computeViewSize(int width, int height) {
+        return new Size(width, height);
+    }
+
+    protected void onPreviewDisplaySurfaceChanged(
+            SurfaceHolder holder, int format, int width, int height) {
+        // (Re-)Compute the ideal size of the camera-preview display (the area that the
+        // camera-preview frames get rendered onto, potentially with scaling and rotation)
+        // based on the size of the SurfaceView that contains the display.
+        Size viewSize = computeViewSize(width, height);
+        Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
+        boolean isCameraRotated = cameraHelper.isCameraRotated();
+
+        // Connect the converter to the camera-preview frames as its input (via
+        // previewFrameTexture), and configure the output width and height as the computed
+        // display size.
+        converter.setSurfaceTextureAndAttachToGLContext(
+                previewFrameTexture,
+                isCameraRotated ? displaySize.getHeight() : displaySize.getWidth(),
+                isCameraRotated ? displaySize.getWidth() : displaySize.getHeight());
+    }
+
     private void setupPreviewDisplayView() {
         previewDisplayView.setVisibility(View.GONE);
         ViewGroup viewGroup = findViewById(R.id.preview_display_layout);
         viewGroup.addView(previewDisplayView);
+
         previewDisplayView
                 .getHolder()
                 .addCallback(
@@ -118,36 +220,19 @@ public class MainActivity extends AppCompatActivity {
                             public void surfaceCreated(SurfaceHolder holder) {
                                 processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
                             }
+
                             @Override
                             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                                // (Re-)Compute the ideal size of the camera-preview display (the area that the
-                                // camera-preview frames get rendered onto, potentially with scaling and rotation)
-                                // based on the size of the SurfaceView that contains the display.
-                                Size viewSize = new Size(width, height);
-                                Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
-                                // Connect the converter to the camera-preview frames as its input (via
-                                // previewFrameTexture), and configure the output width and height as the computed
-                                // display size.
-                                converter.setSurfaceTextureAndAttachToGLContext(
-                                        previewFrameTexture, displaySize.getWidth(), displaySize.getHeight());
+                                onPreviewDisplaySurfaceChanged(holder, format, width, height);
                             }
+
                             @Override
                             public void surfaceDestroyed(SurfaceHolder holder) {
                                 processor.getVideoSurfaceOutput().setSurface(null);
                             }
                         });
     }
-    private void startCamera() {
-        cameraHelper = new CameraXPreviewHelper();
-        cameraHelper.setOnCameraStartedListener(
-                surfaceTexture -> {
-                    previewFrameTexture = surfaceTexture;
-                    // Make the display view visible to start showing the preview. This triggers the
-                    // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
-                    previewDisplayView.setVisibility(View.VISIBLE);
-                });
-        cameraHelper.startCamera(this, CAMERA_FACING, /*surfaceTexture=*/ null);
-    }
+
     private String getMultiHandLandmarksDebugString(List<NormalizedLandmarkList> multiHandLandmarks) {
         if (multiHandLandmarks.isEmpty()) {
             return "No hand landmarks";
@@ -175,4 +260,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return multiHandLandmarksStr;
     }
+
+
 }
